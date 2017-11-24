ec67ef823bac4dbaaec7cc0c4716071faf99db4d
Aleksandr Tereshchenko <terap98@icloud.com>
2017-11-24 20:26:44 +0200

    Added initial schedulers, added initial DB migrations

--- src/main/java/fi/redgrenade/summarizer/schedulers/ArticleSummarizer.java
+++ src/main/java/fi/redgrenade/summarizer/schedulers/ArticleSummarizer.java
@@ -1,7 +1,14 @@
 package fi.redgrenade.summarizer.schedulers;
 
+import org.springframework.scheduling.annotation.Scheduled;
+
 /**
  * Created by aleksandr on 24.11.2017.
  */
 public class ArticleSummarizer {
+
+    @Scheduled(cron = "0 * * * * *")
+    public void summarizeArticles () {
+
+    }
 }
--- src/main/java/fi/redgrenade/summarizer/schedulers/ArticleToKeyConverter.java
+++ src/main/java/fi/redgrenade/summarizer/schedulers/ArticleToKeyConverter.java
@@ -1,7 +1,15 @@
 package fi.redgrenade.summarizer.schedulers;
 
+import org.springframework.scheduling.annotation.Scheduled;
+
 /**
  * Created by aleksandr on 24.11.2017.
  */
+
 public class ArticleToKeyConverter {
+
+    @Scheduled(cron = "0 * * * * *")
+    public void fetchArticles () {
+
+    }
 }
